ec47da1cebc141ab702264421a7dab0aaad2dae6
shfshanyue <wx1144542900@163.com>
2016-10-18 20:57:56 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <rubic :level="3" :width="120"></rubic>
+  </div>
+</template>
+
+<script>
+import Rubic from './components/Rubic'
+
+export default {
+  name: 'app',
+  components: {
+    Rubic
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Rubic.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container">
+    <div class="rubic-container">
+      <div class="rubic">
+        <div
+          class="piece-container"
+          v-for="({ x, y, z }) in accords"
+          :class="`x-${x} y-${y} z-${z}`"
+          :key="`${x}-${y}-${z}`"
+        >
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Cube from './Cube'
+
+export default {
+  name: 'rubic',
+  components: {
+    Cube
+  },
+  props: {
+    level: {
+      type: Number,
+      required: true
+    },
+    animation: {
+      type: Boolean
+    },
+    width: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      msg: 'Welcome to Rubic'
+    }
+  },
+  computed: {
+    accords() {
+      const accords = []
+      for (let x = 0; x < this.level; x++) {
+        for (let y = 0; y < this.level; y++) {
+          for (let z = 0; z < this.level; z++) {
+            accords.push({ x, y, z })
+          }
+        }
+      }
+      return accords
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.rubic-container {
+  height: 600px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.rubic {
+  position: relative;
+}
+
+.piece-container {
+  position: absolute;
+  background-color: #ccc;
+}
+</style>
